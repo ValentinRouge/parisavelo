3218c4e5a4258c2949762756e6e73de6c30a91b4
ValentinRouge <rougevalentin@outlook.com>
2021-04-24 01:58:22 +0100

    popup v1

--- website/public/script.js
+++ website/public/script.js
@@ -2,9 +2,12 @@ const menuBTN = document.querySelector("#menuBTN");
 const navBar = document.querySelector("#navbar");
 const icon = document.querySelector("#icon");
 
+console.log('1')
+
 menuBTN.addEventListener('click', () => {
-    navBar.classList.toggle("hidden")
-    icon.classList.toggle("transition")
-    icon.classList.toggle("transform")
-    icon.classList.toggle("-rotate-90")
+    console.log('test');
+    navBar.classList.toggle("hidden");
+    icon.classList.toggle("transition");
+    icon.classList.toggle("transform");
+    icon.classList.toggle("-rotate-90");
 })
