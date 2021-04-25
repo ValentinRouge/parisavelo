cfc3c82b86ad9d74b9dc9de7c7bdb8dae7efcf7d
ValentinRouge <rougevalentin@outlook.com>
2021-04-25 22:43:51 +0100

    avancer

--- website/public/script.js
+++ website/public/script.js
@@ -1,13 +1,28 @@
-const menuBTN = document.querySelector("#menuBTN");
-const navBar = document.querySelector("#navbar");
-const icon = document.querySelector("#icon");
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBTN = document.querySelector("#menuBTN");
+    const navBar = document.querySelector("#navbar");
+    const icon = document.querySelector("#icon");
+    const legendePOPUP = document.querySelector("#legendePOPUP")
+    const legendeBTN = document.querySelector("#legendeBTN")
+    const closeBTN_legendePOPUP = document.querySelector("#closeBTN_legendePOPUP")
 
-console.log('1')
 
-menuBTN.addEventListener('click', () => {
-    console.log('test');
-    navBar.classList.toggle("hidden");
-    icon.classList.toggle("transition");
-    icon.classList.toggle("transform");
-    icon.classList.toggle("-rotate-90");
+    legendeBTN.addEventListener('click', () => {
+        console.log("test")
+
+        legendePOPUP.classList.remove("hidden")
+
+
+    })
+
+    closeBTN_legendePOPUP.addEventListener('click', () => {
+        legendePOPUP.classList.add("hidden")
+    })
+
+    menuBTN.addEventListener('click', () => {
+        navBar.classList.toggle("hidden");
+        icon.classList.toggle("transition");
+        icon.classList.toggle("transform");
+        icon.classList.toggle("-rotate-90");
+    })
 })
