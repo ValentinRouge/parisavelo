7eeb388ed82381e03d9c36cd914e57ff146b6a18
ValentinRouge <rougevalentin@outlook.com>
2021-04-26 19:15:34 +0100

    nouvelle structure + sw finalisé

--- website/sw.js
+++ website/sw.js
@@ -1,13 +1,59 @@
+const statiCacheName = 'site-static'
+const dynamicCacheName = 'site-dynamic'
+const assets = [
+    '/', 
+    '/index.html',
+    '/script.js',
+    '/styles.css',
+    '/logo/32.png',
+    '/logo/120.png',
+    '/logo/152.png',
+    '/logo/167.png',
+    '/logo/180.png',
+    '/logo/192.png',
+    '/logo/512.png',
+    '/logo/512.ico',
+    '/images/logo_fontaine.png',
+    '/images/logo_parking.png',
+    '/images/logo_reparation.png',
+    '/images/logo_toilettes.png',
+    '/images/logo_velib.png',
+    '/images/logo.svg',
+    '/manifest.json'
+]
+
 // install sw
 self.addEventListener('install', (evt) => {
-    console.log('sw has been installed')
+    evt.waitUntil(
+        caches.open(statiCacheName).then(cache => {
+            console.log('caching shell assets')
+            cache.addAll(assets)
+        })
+    );
 })
  // activate sw
 self.addEventListener('activate', (evt) => {
-    console.log('sw has been activated')
+    evt.waitUntil(
+        caches.keys().then(keys =>{
+            return Promise.all(keys
+                .filter(key => key !== statiCacheName && key !== dynamicCacheName)
+                .map(key => caches.delete(key))    
+            )
+        })
+    )
 })
 
 //fetch event 
 self.addEventListener('fetch', (evt) => {
-    console.log('fetch event', evt)
-})
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes
+                })
+            });
+        })
+    );
+
+});
